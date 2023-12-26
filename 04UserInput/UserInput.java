f8a5399a08b7be4aae34fd44e3579cbbd8816abc
tan05 <tan.tantham.tan@gmail.com>
2023-12-26 21:55:26 +0700

    User Input

---
+++ 04UserInput/UserInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("What is your age? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello " + name + " your are " + age + " years old " + " you like " + food);
+
+    }
+
+}
