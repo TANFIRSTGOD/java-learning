f72570ac32451148aaba408e07dbcb1b082d6456
tan05 <tan.tantham.tan@gmail.com>
2023-12-26 22:10:00 +0700

    Basic gui

---
+++ 05 expressions/expressions.java
@@ -0,0 +1,15 @@
+public class expressions {
+    
+    public static void main(String[] args) {
+        // expressions = operands & operators
+        // operands = values, variables, number, quantity
+        // operators = +-*/%
+
+        int friends = 10;
+
+        
+
+        System.out.println(friends);
+    }
+
+}
